82032eb956ed3684f4c9637ee2b0fc82a637fd26
YacoubKahkajian <81059899+YacoubKahkajian@users.noreply.github.com>
2023-03-04 16:32:40 -0500

    Added backend to open a Google Sheet

---
+++ prince-quiz/server/index.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+const express = require("express");
+const doc = new GoogleSpreadsheet(process.env.SHEET_ID);
+const app = express();
+const PORT = process.env.PORT || 3001;
+let rows;
+let final = [];
+
+(async function() {
+    await doc.useServiceAccountAuth({
+        client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+        private_key: process.env.GOOGLE_PRIVATE_KEY,
+    });
+    await doc.loadInfo(); // loads sheets
+    const sheet = doc.sheetsByIndex[0];
+    rows = await sheet.getRows();
+    build();
+}());
+
+function build() {
+    for (let i = 0; i < rows.length; i++) {
+        let obj = {
+            question: rows[i].question,
+            c1: rows[i].c1,
+            c1img: rows[i].c1img,
+            c2: rows[i].c2,
+            c2img: rows[i].c2img,
+            c3: rows[i].c3,
+            c3img: rows[i].c3img,
+            c4: rows[i].c4,
+            c4img: rows[i].c4img,
+            correct: rows[i].correct
+        };
+        final.push(obj);
+    }
+}
+
+app.get("/api", (req, res) => {
+    res.json(final);
+});
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+});
--- prince-quiz/src/App.tsx
+++ prince-quiz/src/App.tsx
@@ -6,15 +6,18 @@ import './App.css';
 function App() {
   // start with state equal to false (it could also be a string or int depending on what you want) -> every time the state is changed using setIsCorrect(),
   // everything which is dependent on isCorrect is updated
-  const [isCorrect, setIsCorrect] = useState(false)
+  const [data, setData] = useState(null)
 
   // useEffect works such that whenever the thing in the square brackets changes, the function on the left is run
   // if the square bracket space is empty, the function runs on page load.
   // for instance if "i" was in the square brackets, whenever i was altered, the function would run
   useEffect(() => {
     // console.log will be really helpful in debugging, if you double click on the page in the broswer and go to "inspect", then at the top of the pop-up if you click "Console", you'll see the logged stuff come up (everytime the page loads, the below text will show up due to the useEffect) 
-    console.log("hey, I've loaded")
-  }, [])
+    console.log("hey, I've loaded");
+    fetch("/api")
+        .then((res) => res.json())
+        .then((data) => setData(data.message));
+  }, []);
 
   // example of making an array of objects that can be generated on line 24. "push()" adds an item to the end of the array
   let i = []
@@ -30,17 +33,8 @@ function App() {
       {/* use curly brackets to input placeholders */}
       {/* render whole array created above */}
       {i}
-
-      {/* short if statement syntax. The way this is read is, "if isCorrect == true, return question name="correct" etc., else return question name="incorrect" etc */}
-      {/* note that isCorrect is a state variable so if we change it with the onclick button attribute, this condition will be rerun */}
-      {isCorrect ? (
-        <Question name={"Correct!"} id={30}/>
-      ) : (
-        <Question name={"Incorrect"} id={30}/>
-      )}
       
-      {/* change state on click */}
-      <button onClick={() => {setIsCorrect(true)}}>Click me to change to correct!</button>
+      <p>{!data ? "Loading..." : data}</p>
     </div>
   );
 }
